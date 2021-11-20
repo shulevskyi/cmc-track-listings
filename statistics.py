836a13d00137628134616b972f7402e04c2f69ed
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-11-20 12:55:40 +0200

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,1 +1,3 @@
-# Script made to calculate estimated profit since coin appeared in untracked and active listings by finding the most frequent percentage increase by a tokens. Therefore we should get a price at the time when it was listed and the time when it reached maximum, round it to tens, for instance, 48,74..% => 50%. Calculate it for 50+ coins and eventually get the most accurate statics for making a profit for each token.
+# Script made to calculate estimated profit since coin appeared in untracked and active listings by finding the most frequent percentage increase by a tokens.
+# Therefore we should get a price at the time when it was listed and the time when it reached maximum, round it to tens, for instance, 48,74..% => 50%.
+# Calculate it for 50+ coins and eventually get the most accurate statics for making a profit for each token.
